7f17bc4db5875de2a1cecc33e9fd2a9a3372ef3a
maboglia <mauro.bogliaccino@gmail.com>
2023-12-20 10:20:11 +0100

    agg

--- workspace/selenium-test/src/test/java/suite/AllTests.java
+++ workspace/selenium-test/src/test/java/suite/AllTests.java
@@ -5,5 +5,6 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectPackages("tests")
+@SelectPackages("tests")//tutte le classi del/i package/s
+//@SelectClasses({})//singole classi
 public class AllTests {}
